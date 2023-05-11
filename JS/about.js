7ccfd6379ae088f76007dd8db30c4e92be412ae4
Gustavo Ribeiro <gpr.gus@icloud.com>
2023-05-11 10:07:52 -0700

    script to scroll down on click to the link 'about working'

--- JS/about.js
+++ JS/about.js
@@ -1,9 +1,8 @@
-const aboutLink = document.querySelector('.nav-link[href="#about"]');
-aboutLink.addEventListener('click', (event) => {
-  event.preventDefault(); // Prevent the default behavior of the link
-  const offcanvas = document.querySelector('.offcanvas');
-  const navbarHeight = document.querySelector('.navbar').offsetHeight;
-  offcanvas.classList.remove('show'); // Close the offcanvas menu
-  const targetOffset = document.body.scrollHeight - window.innerHeight;
-  window.scrollTo({ top: targetOffset, behavior: 'smooth' }); // Smoothly scroll the page to the bottom
+document.addEventListener('DOMContentLoaded', function() {
+  const aboutLink = document.querySelector('a[href="#about"]');
+  aboutLink.addEventListener('click', function() {
+      const offcanvas = document.querySelector('.offcanvas');
+      const offcanvasInstance = bootstrap.Offcanvas.getInstance(offcanvas);
+      offcanvasInstance.hide();
+  });
 });
